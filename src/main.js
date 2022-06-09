38b66339911e81c8a765728852e2282c9f054f86
Alex <alexander.caspersson@gmail.com>
2022-06-09 13:03:54 +0200

    Final commit

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import router from './router';
 import App from './App.vue';
 import { registerSW } from 'virtual:pwa-register';
+import '/assets/main.scss'
 registerSW();
 
 createApp(App).use(router).mount('#app');
